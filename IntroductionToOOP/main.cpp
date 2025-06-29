c26bfa2ce00ff4f2499e10cee171cd416bda70d1
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-06-29 22:48:05 +0300

    +Graf() - draw coordinate grid

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -41,19 +41,91 @@ public:
 	{
 		this->yy = yy;
 	}
-	double distance(double a,double b)const
+	double distance(double a, double b)const
 	{
-		double gptnz=sqrt(a * a + b * b);
+		double gptnz = sqrt(a * a + b * b);
 		return gptnz;
 	}
-	double distance(double a,double b,double aa,double bb)const
+	double distance(double a, double b, double aa, double bb)const
 	{
 		double aaa, bbb;
 		if (a > aa)aaa = a - aa; else aaa = aa - a;
 		if (b > bb)bbb = b - bb; else bbb = bb - b;
 
-		double gptnz=sqrt(aaa * aaa + bbb * bbb);
+		double gptnz = sqrt(aaa * aaa + bbb * bbb);
 		return gptnz;
+	}
+	void Graf(double x, double y, double xx, double yy)
+	{
+		setlocale(LC_ALL, "C");
+		cout << '\x20' << "Y" << endl;
+		cout << '\x20' << '\x1E' << endl;
+		cout << '\x20' << '\xB3' << endl;
+		for (int i = ((y > yy ? y : yy)+1); i > 0; i--)
+		{
+			
+			if (i == (y +1) && i == (yy + 1))
+			{
+				cout << i << '\xB3';
+				for (int i = 1; i < (x > xx ? x : xx)+1; i++)
+				{
+					if (i == x)
+					{
+						cout << "a";
+						continue;
+					}
+					if (i == xx)
+					{
+						cout << "b";
+						continue;
+					}
+					cout << '\x20';
+				}
+				cout << endl;
+				continue;
+			}
+			if (i == y&&i==yy)
+			{
+				cout << i << '\xB3';
+				for (int i = 1; i < (x > xx ? x : xx)+1; i++)
+				{
+					if (i == x)
+					{
+						cout << '\xFD';
+						continue;
+					}
+					if (i == xx)
+					{
+						cout << '\xFD';
+						continue;
+					}
+					cout << '\x20';
+				}
+				cout << endl;
+				continue;
+			}
+			if (i == y&&i!=yy)
+			{
+				cout << i << '\xB3';
+				for (int i = 0; i < x - 1; i++)cout << '\x20';
+				cout << "\xFD" << "a" << endl;
+				continue;
+			}
+			if (i == yy&&i!=y)
+			{
+				cout << i << '\xB3';
+				for (int i = 0; i < xx - 1; i++)cout << '\x20';
+				cout << "\xFD" << "b" << endl;
+				continue;
+			}
+			cout << i << '\xB3' << endl;
+		}
+		cout << '\x20' << '\xC5';
+		for (int i = 0; i < (x > xx ? x : xx) + 2; i++)cout << '\xC4';
+		cout << '\x10' << "X" << endl << '\x20' << '\x20';
+		for (int i = 1; i < (x > xx ? x : xx) + 2; i++)cout << i;
+		cout << endl;
+		setlocale(LC_ALL, "");
 	}
 };
 
@@ -64,10 +136,10 @@ void main()
 	setlocale(LC_ALL, "");
 #ifdef SRTUCT_POINT
 	Point A; // Объявление переменной 'А' типа Point
-			// Создание объекта 'А' структуры Point
-			// Создание экземпляра 'А' структуры Point
-			// 'A' is instance of struct 'Point'
-			// Instantiate - создать объект.
+	// Создание объекта 'А' структуры Point
+	// Создание экземпляра 'А' структуры Point
+	// 'A' is instance of struct 'Point'
+	// Instantiate - создать объект.
 	A.x = 2;
 	A.y = 3;
 	cout << A.x << "\t" << A.y << endl;
@@ -78,15 +150,21 @@ void main()
 
 	Point A;
 	double x, y, xx, yy;
-	cout << "Введите координаты первой точки (x,y): "; cin >> x >> y;
+	cout << "Введите координаты первой точки  (x,y) : "; cin >> x >> y;
 	cout << "Введите координаты второй точки (xх,yу): "; cin >> xx >> yy;
-	A.set_x(x);
-	A.set_y(y);
-	A.set_xx(xx);
-	A.set_yy(yy);
+	/*if (x != 0 && y != 0)
+	{*/
+		A.set_x(x);
+		A.set_y(y);
+	/*}
+	if (xx != 0 && yy != 0)//хотел перегрузить
+	{*/
+		A.set_xx(xx);
+		A.set_yy(yy);
+	//}
 
-	cout << A.get_x() << "\t" << A.get_y() << endl;
-	
-	cout << "Расстояние до точки: " << A.distance(A.get_x(), A.get_y())<<endl;
-	cout << "Расстояние между точками: " << A.distance(A.get_x(), A.get_y(), A.get_xx(), A.get_yy()) << endl;
+	//cout << A.get_x() << "\t" << A.get_y() << endl;
+	A.Graf(A.get_x(), A.get_y(), A.get_xx(), A.get_yy());
+	cout << "Расстояние от нуля до точки: " << A.distance(A.get_x(), A.get_y()) << endl;
+	cout << "Расстояние между точками   : " << A.distance(A.get_x(), A.get_y(), A.get_xx(), A.get_yy()) << endl;
 }
