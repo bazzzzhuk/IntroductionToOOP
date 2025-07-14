c39c2ce7481f793f4a6d6d81d50db9d11f249af3
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-07-14 19:39:07 +0300

    ClassString start

---
+++ ClassString/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+using namespace std;
+
+class String
+{
+	int size;//размер строки в байтах (с учётом NULL-Terminator)
+	char* str;//адрес строки в динамической памяти
+public:
+	//   constructor   //
+	explicit String(int size = 80)
+	{
+		//создаёт пустую строку 80 байт
+		this->size = size;
+		this->str = new char[size] {};
+	}
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1; // функция возвращает размер строки в символах,
+		//+1 нужен чтобы выделилась память ппод нуль-терминатед
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = str[i];
+		cout << "Constructor:\t\t" << this << endl;
+
+	}
+	// Methods
+	void print()const
+	{
+		cout << "Size: \t" << size << endl;
+		cout << "Str: \t" << str << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	String str1;
+	str1.print();
+
+	String str2(5);
+	str2.print();
+
+	String str3 = "Hello";
+	str3.print();
+}
