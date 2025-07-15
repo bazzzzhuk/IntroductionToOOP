f60575ef0e628640e8588a1b9935655c06aaef70
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-07-15 21:47:20 +0300

    Copy Assignment Done

--- ClassString/main.cpp
+++ ClassString/main.cpp
@@ -32,7 +32,7 @@ public:
 	String(const char* str)
 	{
 		this->size = strlen(str) + 1; // функция возвращает размер строки в символах,
-									 //+1 нужен чтобы выделилась память под NULL-terminator
+		//+1 нужен чтобы выделилась память под NULL-terminator
 		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
@@ -48,7 +48,16 @@ public:
 		{
 			this->str[i] = other.str[i];
 		}
-		cout << "CopyConstructor:\t\t" << this << endl;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	String& operator=(String& other)
+	{
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size + 1] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
 	}
 	~String() {
 		delete[] str;
@@ -65,11 +74,11 @@ public:
 };
 String operator+(const String& left, const String& right)
 {
-	String rezult (left.get_size() + right.get_size()-1);
+	String rezult(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
 		rezult.get_str()[i] = left.get_str()[i];
 	for (int i = 0; i < right.get_size(); i++)
-		rezult.get_str()[i+left.get_size()-1] = right.get_str()[i];
+		rezult.get_str()[i + left.get_size() - 1] = right.get_str()[i];
 	return rezult;
 }
 std::ostream& operator<<(std::ostream& os, const String& obj)
@@ -101,6 +110,9 @@ void main()
 	String str3 = "World";
 	cout << str1 << endl;
 	String str2 = str1;
-	cout << str2 << endl;
-	cout << str1 +" " + str3 << endl;
+	cout << "str2= " << str2 << endl;
+	str2 = str3;
+	cout << "str2= " << str2 << endl;
+
+
 }
